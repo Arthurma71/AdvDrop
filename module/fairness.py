be2c515436edafd3f401250031a3f0375e035949
Arthur <957980613@qq.com>
2023-01-12 11:37:20 +0000

    CFC framework

---
+++ module/fairness.py
@@ -0,0 +1,27 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+
+
+
+
+class Filter(nn.Module):
+    def __init__(self, args):
+        super().__init__()
+        # TODO: Modify M with Attention
+        pass
+
+    def forward(self):
+        pass
+
+
+
+class Discriminator(nn.Module):
+    def __init__(self, args):
+        super().__init__()
+        # TODO: Modify M with Attention
+        pass
+
+    def forward(self):
+        pass
+
